be2303d40f0f96a82b42a0067a45e6e793802596
Ondřej Fuchs <o.fuchs@seznam.cz>
2018-05-17 12:24:47 +0200

    firstStart.sh

--- firstStart.sh
+++ firstStart.sh
@@ -6,14 +6,16 @@
 # Wi-Fi and SSH configuration
 cp wpa_supplicant.conf /media/pc/boot/
 cp ssh /media/pc/boot/
-#ID1="c7cb7e34"
-SUBSTRING=$(sudo fdisk -l /dev/sdc | grep '^Disk identifier:')
-stringarray=($SUBSTRING)
-ID1=${stringarray[2]:2}
 # Set partition
 lsblk
 read -e -p "Set partition (sda, sdb, sdc ect.): " -i "sdc" part
-echo "/dev/"$part
+part3=$(echo $part'3')
+#echo $part3
+
+SUBSTRING=$(sudo fdisk -l /dev/$part | grep '^Disk identifier:')
+stringarray=($SUBSTRING)
+ID1=${stringarray[2]:2}
+
 # Resize rootfs partition 
 sudo parted -m /dev/$part print free
 read -e -p "Set end: " -i "4000" end
@@ -21,12 +23,12 @@ sudo parted -m /dev/$part resizepart 2 Yes $end"MB"
 sudo resize2fs /dev/$part"2" $end"M"
 
 # Make new partition with XFS file system
-sudo parted -m /dev/sdc print free
+sudo parted -m /dev/$part print free
 read -e -p "Set start (MB): " -i "4001" start
 read -e -p "Set end (GB): " -i "15,6" end
-sudo parted -m /dev/sdc mkpart primary $start"MB" $end"GB"
-sudo mkfs.xfs -f /dev/sdc3
-SUBSTRING=$(sudo fdisk -l /dev/sdc | grep '^Disk identifier:')
+sudo parted -m /dev/$part mkpart primary $start"MB" $end"GB"
+sudo mkfs.xfs -f /dev/$part3
+SUBSTRING=$(sudo fdisk -l /dev/$part | grep '^Disk identifier:')
 stringarray=($SUBSTRING)
 ID2=${stringarray[2]:2}
 # After changes we must edit cmdline.txt and fstab
@@ -43,15 +45,19 @@ do
   echo $line > /media/pc/boot/cmdline.txt
 done <$input 
 # Mount new partition to /mnt/xfsdata
-SUBSTRING=$(blkid /dev/sdc3)
-stringarray=($SUBSTRING)
-one=${stringarray[1]:5}
-UUID=$(echo "${one//\"}")
-echo "UUID=$UUID"  "/mnt/xfsdata/ xfs defaults 0 0" >> /media/pc/rootfs/etc/fstab
+SUBSTRING=$(blkid /dev/$part3)
+#stringarray=($SUBSTRING)
+#one=${stringarray[1]:5}
+#UUID=$(echo "${one//\"}")
+#echo "UUID=$UUID"  "/mnt/xfsdata/ xfs defaults 0 0" >> /media/pc/rootfs/etc/fstab
+sudo cryptsetup -c aes-xts-plain64 -s 512 -q luksFormat /dev/$part3 --key-file keyfile
 mkdir /media/pc/rootfs/mnt/xfsdata
 # Dataplicity
-#sudo cp mass-install-dp /media/pc/rootfs/etc/network/if-up.d/
-#sudo chmod 755 /media/pc/rootfs/etc/network/if-up.d/mass-install-dp
+sudo cp mass-install-dp /media/pc/rootfs/etc/network/if-up.d/
+sudo chmod 755 /media/pc/rootfs/etc/network/if-up.d/mass-install-dp
+echo "allow-hotplug lo" >> /media/pc/rootfs/etc/network/interfaces
+echo "iface lo inet loopback" >> /media/pc/rootfs/etc/network/interfaces
+echo "Finish setup OS."
 
 
 
